658e93ef1e201669ee6753caa43d5af28fa5065f
Dylan White <dylan.whitej@gmail.com>
2021-02-09 17:57:11 -0800

    adds hideous animations

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ export default class Header extends Component {
     render() {
         return (
             <header>
-                <h1>Dylan's Glorious Horned Animal Sorter Wow</h1>
+                <h1>Dylan's Glorious Horned Animal Sorter <span className="expand">Wow</span></h1>
             </header>
         )
     }
--- src/ImageItem.js
+++ src/ImageItem.js
@@ -4,13 +4,16 @@ export default class ImageItem extends Component {
     render() {
         const creature = this.props.creature;
         return (
-            <div className="imageItem" key={creature.keyword}>
-                <div>
-                <h3>{creature.title}</h3>
-                <p># of horns: <span className="orange">{creature.horns}</span></p>
+            <div className="container">
+                <div className="imageItem" key={creature.keyword}>
+                    <div>
+                        <h3>{creature.title}</h3>
+                        <p># of horns: <span className="orange">{creature.horns}</span></p>
+                    </div>
+                    <img src={creature.url} alt={creature.description} />
                 </div>
-                <img src={creature.url} alt={creature.description} />
             </div>
+            
         )
     }
 }
